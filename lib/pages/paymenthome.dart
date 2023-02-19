a2828b207251ce5cc5f8d6064c4aed474a597694
khushiabp <80744799+khushiabp@users.noreply.github.com>
2023-02-20 01:54:25 +0530

    Update paymenthome.dart

--- lib/pages/paymenthome.dart
+++ lib/pages/paymenthome.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -36,6 +38,8 @@ class _HomePageState extends State<HomePage> {
   void _handleExternalWallet(ExternalWalletResponse response) {
     // Do something when an external wallet is selected
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -77,14 +81,45 @@ class _HomePageState extends State<HomePage> {
                     'external': { 'wallets': ['paytm'] }
                   };
                   _razorpay.open(options);
+                  _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, (PaymentSuccessResponse response) {
+                    String transactionId = response.paymentId.toString();
+                    String orderID = response.orderId.toString();
+                    String signature = response.signature.toString();
+
+                    // Store transaction details in Firebase database
+                    storeTransactionDetails(transactionId, orderID, signature);
+                  });
+
+                  _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, (PaymentFailureResponse response) {
+                    String code = response.code.toString();
+                    storeTransactionFailureDetails(code);
+                    // Handle error
+                  });
                 })
           ],
         ),
       ),
     );
   }
+  void storeTransactionDetails(String transactionId, String orderID, String signature) {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  @override
+    firestore.collection('transactions').add({
+      'transactionId': transactionId,
+      'orderId': orderID,
+      'signature' : signature
+    });
+    }
+
+    void storeTransactionFailureDetails(String code){
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      firestore.collection('failedtransactions').add({
+        'code':code
+      });
+    }
+
+    @override
   void dispose() {
     // TODO: implement dispose
     _razorpay.clear();
